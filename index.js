b401e5ee944496fb15fb68da92f3e06b9390f699
M Haidar Hanif <git@mhaidarhanif.com>
2025-10-10 14:13:35 +0700

    feat: leads data

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+const leads = [
+  {
+    id: 1,
+    name: "Blueberry",
+    phone: "+1234567890",
+    email: "blueberry@example.com",
+  },
+  {
+    id: 2,
+    name: "Strawberry",
+    phone: "+12345678911",
+    email: "strawberry@example.com",
+  },
+];
+
+console.log(leads);
