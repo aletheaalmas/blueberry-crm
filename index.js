8229550588ae3cb35e84db4c57497a8b4a367203
M Haidar Hanif <git@mhaidarhanif.com>
2025-10-10 14:14:43 +0700

    feat: address object

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const leads = [
     name: "Blueberry",
     phone: "+1234567890",
     email: "blueberry@example.com",
+    address: {
+      city: "Jakarta",
+      country: "Indonesia",
+    },
   },
   {
     id: 2,
     name: "Strawberry",
     phone: "+12345678911",
     email: "strawberry@example.com",
+    address: {
+      city: "Bandung",
+      country: "Indonesia",
+    },
   },
 ];
 
