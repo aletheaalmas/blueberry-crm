d00cdd861949882bac921f320cc67720c781df6a
M Haidar Hanif <git@mhaidarhanif.com>
2025-11-12 14:54:17 +0700

    handle say hello

--- 2025-11-12/index.js
+++ 2025-11-12/index.js
@@ -1,3 +1,7 @@
 const buttonSayHelloElement = document.getElementById("say-hello");
 
-console.log(buttonSayHelloElement);
+function handleSayHello() {
+  alert("Hello");
+}
+
+buttonSayHelloElement.onclick = handleSayHello;
