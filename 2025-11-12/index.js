37fc369f263d01bd67ae0af98123affee2ec46f9
M Haidar Hanif <git@mhaidarhanif.com>
2025-11-12 14:52:14 +0700

    experiement button event

--- 2025-11-12/index.js
+++ 2025-11-12/index.js
@@ -1,32 +1,3 @@
-function saveToStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
+const buttonSayHelloElement = document.getElementById("say-hello");
 
-// ---
-
-const getExampleProducts = async () => {
-  const response = await fetch("https://fakestoreapi.com/products");
-  const products = await response.json();
-
-  return products;
-};
-
-getExampleProducts().then((products) => {
-  saveToStorage("products", products);
-});
-
-// ---
-
-const getExampleUsers = async () => {
-  const response = await fetch("https://fakestoreapi.com/users");
-  const users = await response.json();
-
-  return users;
-};
-
-const fetchAndSave = async () => {
-  const users = await getExampleUsers();
-  saveToStorage("users", users);
-};
-
-fetchAndSave();
+console.log(buttonSayHelloElement);
