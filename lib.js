8ca7f99e1d0225ad0641b7b70585fe31ba3dd3d3
Alethea Almas <alethea.almas@gmail.com>
2025-11-18 13:23:48 +0800

    add getInitial, fullName and statusColor function

--- lib.js
+++ lib.js
@@ -5,3 +5,36 @@ function getAmmountARR(annualRevenueInUSD) {
       : "N/A";
   return amountARR;
 }
+
+function getInitial(firstName, lastName) {
+  const first = firstName?.[0] ?? "";
+  const last = lastName?.[0] ?? "";
+  return (first + last).toUpperCasse() || "N/A";
+}
+
+function getFullName(lead) {
+  return `${lead.firstName ?? ""} ${lead.lastName ?? ""}`.trim();
+}
+
+function getStatusColor(status) {
+  switch (status) {
+    case "New":
+      return "bg-stone-600";
+      break;
+    case "Contacted":
+      return "bg-yellow-600";
+      break;
+    case "Qualified":
+      return "bg-indigo-600";
+      break;
+    case "Nurtured":
+      return "bg-green-600";
+      break;
+    case "Junk":
+      return "bg-red-600";
+      break;
+    default:
+      return "";
+      break;
+  }
+}
